96bce73ce45e7b6cfa9902a5a23a2acf4b6e8a5d
n4ctbyte <christiannakata9392@gmail.com>
2025-08-29 12:04:51 +0700

    fix kesalahan berpikir

--- Main.java
+++ Main.java
@@ -2,27 +2,19 @@ package Praktikum;
 
 public class Main {
     public static void main(String[] args) {
-        // membuat objek mahasiswa
-        Mahasiswa nakata = new Mahasiswa("Nakata Christian", 2407112366L, new double[] {3.62, 3.67}, "Jalan Riau Ujung", 20, 'O');
-    }
-}
+        String nama1 = "Nakata Christian";
+        final long nim1 = 2407112366L;
+        double[] ip1 = {3.62, 3.67};
+        String alamat1 = "Jalan Riau Ujung";
+        int umur = 20;
+        char golongan_darah = 'O';
 
-// template agar tidak usah mendeklarasikan variabel berulang ulang untuk setiap mahasiswa
-class Mahasiswa {
-    String nama;
-    long nim;
-    double[] ip;
-    String alamat;
-    int umur;
-    char golongan_darah;
-
-    // constructor untuk mengisi data saat membuat objek
-    public Mahasiswa(String nama, long nim, double[] ip, String alamat, int umur, char golongan_darah) {
-        this.nama = nama;
-        this.nim = nim;
-        this.ip = ip;
-        this.alamat = alamat;
-        this.umur = umur;
-        this.golongan_darah = golongan_darah;
+        System.out.println("=== BIODATA NAKATA ===");
+        System.out.println("Nama : " + nama1);
+        System.out.println("NIM : " + nim1);
+        System.out.println("IP Semester 1 " + ip1[0]);
+        System.out.println("Alamat : " + alamat1);
+        System.out.println("Umur : " + umur);
+        System.out.println("Golongan Darah : " + golongan_darah);
     }
 }
