0f73b9be3c9799ca61c0c05b9f4425cdcc90be02
asmaelhasna <163143921+asmaelhasna@users.noreply.github.com>
2025-09-07 14:04:21 +0700

    Add files via upload

---
+++ PrakPbo3.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ppbo01;
+
+/**
+ *
+ * @author ASUS
+ */
+import java.util.Scanner; //mengimpor library scanner untuk input user
+
+public class PrakPbo3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); //deklarasi dari objek scanner agar input user masuk
+        
+        //tampilan awal program
+        System.out.println("========================================================");
+        System.out.println("   KALKULATOR BANGUN RUANG PRISMA TRAPESIUM (cm³)      ");
+        System.out.println("========================================================");
+        
+        //input nilai dari prisma trapesium
+        System.out.print("Masukkan sisi sejajar 1 (cm): ");
+        double sisisejajar1 = scanner.nextDouble();
+        System.out.print("Masukkan sisi sejajar 2 (cm): ");
+        double sisisejajar2 = scanner.nextDouble();
+        System.out.print("Masukkan tinggi alas trapesium (cm): ");
+        double tinggiAlasTrapesium = scanner.nextDouble();
+        System.out.print("Masukkan tinggi prisma (cm): ");
+        double tinggiPrisma = scanner.nextDouble();
+
+        //hitung luas alas dari trapesium
+        double luasAlas = ((sisisejajar1 + sisisejajar2) * tinggiAlasTrapesium) / 2;
+
+        //hitung volume dari prisma trapesium
+        double volume = luasAlas * tinggiPrisma;
+
+        //output hasil luas alas dan volume prisma trapesium
+        System.out.println("========================================================");
+        System.out.println("Luas alas trapesium: " + luasAlas + " cm²");
+        System.out.println("Volume prisma trapesium adalah: " + volume + " cm³");
+        System.out.println("========================================================");
+
+        scanner.close(); //scanner ditutup karena input sudah selesai
+    }
+}
