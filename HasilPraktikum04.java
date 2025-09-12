5af8f2aea08436f80dcff13230072d63782d2771
asmaelhasna <163143921+asmaelhasna@users.noreply.github.com>
2025-09-13 02:02:54 +0700

    Update HasilPraktikum04.java

--- HasilPraktikum04.java
+++ HasilPraktikum04.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.ppbo;
 
 import java.util.Scanner;
@@ -142,3 +138,4 @@ public class HasilPraktikum04 {
     }
 
 }
+
